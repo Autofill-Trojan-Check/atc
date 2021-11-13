aecf86d9af990bf895f9aff9e25384fd5f2dc719
Ariana Goldstein <arianang@usc.edu>
2021-11-13 14:51:44 -0800

    javascript update

--- extension/popup.js
+++ extension/popup.js
@@ -1,18 +1,27 @@
 // what the extension is supposed to do
 
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("page loaded, in first event listener");
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost/phpchrome.php", true);
-    xhr.onreadystatechange = function() {
-
-        if (xhr.readyState == 4) {
-            varxhrjson = JSON.parse(xhr.responseText);
-        }
+    var val = document.getElementById('submitbtn');
+    if (val) {
+        val.addEventListener('click', function(event) {
+            event.preventDefault();
+            var username = document.getElementById('username').value;
+            var phonenumber = document.getElementById('phonenumber').value;
+            var password = document.getElementById('password').value;
+        
+            console.log("submit button clicked")
+            console.log(username);
+            console.log(phonenumber);
+            console.log(password);
+        
+            // chrome.tabs.executeScript({
+            //     code: `var value = $(username);`
+            // }, function() {
+            //     127.0.0.1:5000/
+            // }
+            //)
+        });
     }
-    xhr.send();
 });
-
-function onSubmitFunction() {
-    console.log("we're in the onSubmit function!");
-}
